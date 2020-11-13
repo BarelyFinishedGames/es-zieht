8b83b04b31dd1c29c4c456a03a5dc690cf812076
tufteddeer <tufteddeer@disroot.org>
2020-11-13 21:45:36 +0100

    basic player movement

---
+++ player/player.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+var direction = Vector2(0,0)
+
+export(float) var velocity = 3.0
+
+func _physics_process(delta):
+
+	direction = Vector2()
+
+	if Input.is_action_pressed("move_up"):
+		direction += Vector2.UP
+
+	if Input.is_action_pressed("move_left"):
+		direction += Vector2.LEFT
+
+	if Input.is_action_pressed("move_down"):
+		direction += Vector2.DOWN
+
+	if Input.is_action_pressed("move_right"):
+		direction += Vector2.RIGHT
+
+
+	move_and_collide(direction.normalized() * velocity * delta)
