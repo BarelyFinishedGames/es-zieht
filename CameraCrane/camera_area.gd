a626e4e0564fd9490505007426ff53a3a5c8ee74
tufteddeer <tufteddeer@disroot.org>
2020-11-15 00:01:54 +0100

    make CameraCrane in CameraArea accessible through editor

--- CameraCrane/camera_area.gd
+++ CameraCrane/camera_area.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+export(NodePath) var camera_crane
+
 func _on_CameraArea_body_entered(other):
 	if other.has_method("is_player"):
-		get_parent().get_node("CameraCrane").move_to(position)
+		get_node(camera_crane).move_to(position)
