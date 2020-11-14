6d6f28053a734dfb8d79d137fa73fb01ca24a551
tufteddeer <tufteddeer@disroot.org>
2020-11-14 03:07:32 +0100

    implement airflow interruption
    Co-authored-by: Flo0807 <60519307+Flo0807@users.noreply.github.com>

--- box/box.gd
+++ box/box.gd
@@ -1,5 +1,8 @@
 extends RigidBody2D
 
+func is_box():
+	return true
+
 func _physics_process(state):
 	rotation =  0
 	linear_velocity = Vector2(0,0)
--- stream/stream.gd
+++ stream/stream.gd
@@ -1,7 +1,44 @@
 extends Node2D
 
+onready var ray = $begin/BoxDetector
+onready var areaShape = $Area2D/CollisionShape2D
 onready var direction = ($end.get_global_position() - $begin.get_global_position()).normalized()
+
+onready var originalEndPosition = $end.position
+
+
 export(float) var power = 2
+
+func _physics_process(delta):
+
+	if ray.is_colliding():
+		var other = ray.get_collider() as Node2D
+		if other.has_method("is_box"):
+
+			var pos = ray.get_collision_point()
+			$end.global_position = pos
+
+			var length = ($end.position - $begin.position).length()
+			var oldExtents = areaShape.shape.get_extents()
+
+			areaShape.shape.set_extents(Vector2(oldExtents.x,length / 2))
+			areaShape.position.y -= oldExtents.y - length / 2
+
+		else:
+			reset()
+
+	else:
+		reset()
+
+func reset():
+	$end.position = originalEndPosition
+
+	var length = ($end.position - $begin.position).length()
+	var oldExtents = areaShape.shape.get_extents()
+
+	areaShape.shape.set_extents(Vector2(oldExtents.x,length / 2))
+	areaShape.position.y += length / 2 - oldExtents.y
+	
 
 func _on_Area2D_body_entered(other):
 	if other.has_method("is_player"):
