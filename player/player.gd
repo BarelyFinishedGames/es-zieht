a02a28e4d9470eacd60ccff1877e1ba1a18b6c34
Flo0807 <flo.are0807@gmail.com>
2020-11-15 18:34:15 +0100

    Add step and drag sound

--- player/player.gd
+++ player/player.gd
@@ -43,6 +43,12 @@ func _physics_process(delta):
 	else:
 		$Sprite.stop()
 	
+	if direction.length() != 0 and !$Step.playing and !dragging:
+		$Step.play()
+	
+	if direction.length() != 0 and !$Drag.playing and dragging:
+		!$Drag.play()
+	
 	direction += force
 	
 	var allow_turn = true
