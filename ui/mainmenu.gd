c7640524c14a9a6be8c7ab2493624b34dda61fb3
tufteddeer <tufteddeer@disroot.org>
2020-11-16 10:17:34 +0100

    fix level transitions
    TIL preloading packed scenes is a no-no, so the level_transition script
    is now build into the nodes which are now lokal to the scenes so we can
    hardcode the destination level manually for every transition
    
    also the scene to load when the game is started from the main menu is
    replaced with a resource url, which makes the builds working outside the
    project directory

--- ui/mainmenu.gd
+++ ui/mainmenu.gd
@@ -1,9 +1,7 @@
 extends TextureRect
 
-export(PackedScene) var firstLevel
-
 func _on_btExit_button_down():
 	get_tree().quit()
 
 func _on_btStart_button_down():
-	get_tree().change_scene("./tutorial.tscn")
+	get_tree().change_scene("res://tutorial.tscn")
